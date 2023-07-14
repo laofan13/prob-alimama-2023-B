20dce0280576222f525f06f523e037fba1d1df46
lifengfan <2495518995@qq.com>
2023-07-15 01:06:42 +0800

    local develop branch init

--- code/alimama/alimama_search_service.cc
+++ code/alimama/alimama_search_service.cc
@@ -10,6 +10,7 @@
 #include <vector>
 #include <memory>
 #include <string>
+#include <chrono>
 
 #include <arpa/inet.h>
 #include <ifaddrs.h>
@@ -107,11 +108,13 @@ Options loadENV() {
     return options;
 }
 
+
 // ETCD 
 const std::string ECTD_URL = "http://etcd:2379";
 const std::string modelservice_key = "/services/searchservice";
 const std::string filename = "/data/raw_data.csv";
 const size_t initialSize = 1e5;
+constexpr size_t buf_size = 1 << 30;
 
 class SearchServiceImpl final : public SearchService::Service {
 public:
@@ -119,10 +122,9 @@ public:
         options_(options),
         server_address_(server_address),
         etcd_(ECTD_URL),
-        hash_tables(initialSize),
-        data_num_(0)
+        hash_tables(initialSize)
     {
-            
+        
     }
 
     ~SearchServiceImpl() {
@@ -130,22 +132,34 @@ public:
     }
 
     int init() {
-        if(load_data() == -1) {
+         // 记录开始时间
+        auto start = std::chrono::high_resolution_clock::now();
+        if(load_data() != 0) {
             std::cout << "fiald to load data " << filename << std::endl;
             return -1;
         }
+        //   // 记录结束时间
+        auto end = std::chrono::high_resolution_clock::now();
+        
+        // 计算读取文件所花费的时间（以毫秒为单位）
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+        // 输出读取的文件内容和所花费的时间
+        std::cout << "读取文件所花费的时间：" << duration << " 毫秒" << std::endl;
+        std::cout << "文件行数：" << data_num << std::endl;
 
         // for(auto & pair: hash_tables) {
         //     std::cout << "keyword:" << pair.first << std::endl;
         //     for(auto & data: pair.second) {
         //         std::cout  << "adgroup_id: " << data.adgroup_id << " | "
         //                 << "price: " << data.price << " | "
-        //                 << "timings: " << std::hex << data.timings << std::dec << " | "
+        //                 << "timings_mask: " << std::hex << data.timings_mask << std::dec << " | "
         //                 << "vector: ["<< data.vec1 << ", "<< data.vec2 << "]"
         //                 << std::endl;
         //     }
         //     std::cout <<std::endl;
         // }
+
+
         // 将服务地址注册到etcd中
         auto response = etcd_.set(modelservice_key, server_address_).get();
         if (response.is_ok()) {
@@ -155,6 +169,122 @@ public:
                     << "\n";
             return -1;
         }
+        return 0;
+    }
+
+private:
+    int load_data() {
+        std::ifstream csv_file(filename);
+        char *buffer = new char[buf_size];
+        csv_file.rdbuf()->pubsetbuf(buffer, buf_size);
+
+        if (!csv_file.is_open()) {
+            std::cout << "fiald to open " << filename << std::endl;
+            delete [] buffer;
+            return -1;
+        }
+
+
+        int64_t key_word = 0;
+        std::string line, cell;
+        while (std::getline(csv_file, line)) {
+            int n = line.size();
+            int i = 0;
+            key_word = 0;
+            RawData rawData;
+            // keywrod
+            while(i < n && line[i] != '\t') {
+                key_word = key_word * 10 + (line[i] - '0');
+                i++;
+            }
+
+            // load 
+            // if(key_word % options_.node_num != options_.node_id) 
+            //     continue;
+
+            while(i < n && line[i] == '\t')
+                i++; 
+            // adgroup_id
+            rawData.adgroup_id = 0;
+            while(i < n && line[i] != '\t') {
+                rawData.adgroup_id = rawData.adgroup_id * 10 + (line[i] - '0');
+                i++;
+            }
+            while(i < n && line[i] == '\t')
+                i++;
+
+            // price
+            rawData.price = 0;
+            while(i < n && line[i] != '\t') {
+                rawData.price = rawData.price * 10 + (line[i] - '0');
+                i++;
+            }
+            while(i < n && line[i] == '\t')
+                i++;
+
+            // status
+            if(line[i] == '0') {
+                continue;
+            }
+            i++;
+            while(i < n && line[i] == '\t')
+                i++;
+
+            // timings
+            rawData.timings_mask = 0;
+            int k = 0;
+            while(i < n) {
+                if(line[i] == '0') {
+                    k++;
+                }else if(line[i] == '1') {
+                    rawData.timings_mask |= 1 << k;
+                    k++;
+                }
+                i++;
+                if(k >= 24) 
+                    break;
+            }
+            while(i < n && line[i] == '\t')
+                i++;
+            // vector1
+            rawData.vec1 = 0.0f;
+            float decimal = 0.1f;
+            while (i < n && line[i] != '.') {
+                rawData.vec1 = rawData.vec1 * 10.0f + (line[i] - '0');
+                ++i;
+            }
+            ++i;
+            while (i < n && line[i] != ',') {
+                rawData.vec1 = rawData.vec1 + (line[i] - '0') * decimal;
+                decimal *= 0.1f;
+                ++i;
+            }
+            i++;
+            // vector1
+            rawData.vec2 = 0.0f;
+            decimal = 0.1f;
+            while (i < n && line[i] != '.') {
+                rawData.vec2 = rawData.vec2 * 10.0f + (line[i] - '0');
+                ++i;
+            }
+            ++i;
+            while (i < n && line[i] != '\t') {
+                rawData.vec2 = rawData.vec2 + (line[i] - '0') * decimal;
+                decimal *= 0.1f;
+                ++i;
+            }
+
+            if(hash_tables.find(key_word) == hash_tables.end()) {
+                hash_tables[key_word] = {};
+            }
+            hash_tables[key_word].push_back(std::move(rawData));
+            data_num++;
+        }
+
+        // free buf
+        delete [] buffer;
+        // 关闭文件
+        csv_file.close();
         return 0;
     }
 
@@ -175,7 +305,7 @@ public:
             auto keyword = request->keywords(i);
             if(hash_tables.find(keyword) != hash_tables.end()) {
                 for(auto & data: hash_tables[keyword]) {
-                    if(data.timings & (1 << hour)) {
+                    if(data.timings_mask & (1 << hour)) {
                         searchDatas.push_back(data);
                         indexs.push_back(j++);
                     }
@@ -227,75 +357,20 @@ public:
     }
 
 private:
-    int load_data() {
-        std::ifstream csv_file(filename);
-
-        if (!csv_file.is_open()) {
-            std::cout << "fiald to open " << filename << std::endl;
-            return -1;
-        }
-
-        int64_t key_word = 0;
-        int status = 0; 
-        std::string line, cell;
-        while (std::getline(csv_file, line)) {
-            std::istringstream iss(line);
-            RawData rawData;
-            // keywrod
-            iss >> cell;
-            key_word = std::stoull(cell);
-            // adgroup_id
-            iss >> cell;
-            rawData.adgroup_id = std::stoull(cell);
-            // price
-            iss >> cell;
-            rawData.price = std::stoul(cell);
-            // status
-            iss >> cell;
-            status = std::stoi(cell);
-            if(status == 0)
-                continue;
-            // timings
-            iss >> cell;
-            rawData.timings = 0;
-            int k = 0;
-            for(int i = 0; i < cell.size(); i++) {
-                if(cell[i] == '0') {
-                    k++;
-                }else if(cell[i] == '1') {
-                    rawData.timings |= 1 << k;
-                    k++;
-                }
-            }
-    
-            iss >> cell;
-            int pos = cell.find_first_of(',');
-            rawData.vec1 = std::stof(cell.substr(0, pos));
-            rawData.vec2 = std::stof(cell.substr(pos + 1));
-    
-            if(hash_tables.find(key_word) == hash_tables.end()) {
-                hash_tables[key_word] = {};
-            }
-            hash_tables[key_word].push_back(rawData);
-            data_num_++;
-        }
-        return 0;
-    }
-
-private:
     Options options_;
     std::string server_address_;
     etcd::Client etcd_;
 
+    //byte size = 20B
     struct RawData {
         uint64_t adgroup_id;
         uint16_t price;
-        int32_t timings;
+        int32_t timings_mask;
         float vec1, vec2;
     };
 
     std::unordered_map<int64_t, std::vector<RawData>> hash_tables;
-    uint64_t data_num_;
+    uint64_t data_num = 0;
 };
 
 void RunServer() {
--- run-local-test.sh
+++ run-local-test.sh
@@ -14,7 +14,7 @@ cd `dirname $0`
 cp -rf code build
 #考生相关: build.sh是构建入口脚本, run.sh是运行入口脚本
 docker run --rm -v /`pwd`/build://work -w //work --network none public-images-registry.cn-hangzhou.cr.aliyuncs.com/public/alimama-2023:B-v1 bash build.sh
-docker run --rm -v /`pwd`/testbench://work -w //work --network none public-images-registry.cn-hangzhou.cr.aliyuncs.com/public/alimama-2023:B-v1 bash build.sh
+# docker run --rm -v /`pwd`/testbench://work -w //work --network none public-images-registry.cn-hangzhou.cr.aliyuncs.com/public/alimama-2023:B-v1 bash build.sh
 
 echo "3-Run"
 cd `dirname $0`
@@ -23,4 +23,4 @@ cp -rp build node-2
 cp -rp build node-3 
 docker compose down
 docker compose up -d
-docker compose logs -f testbench
+# docker compose logs -f testbench
