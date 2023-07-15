2995f49d34262495168c0a2e6622fdca0e717212
lifengfan <2495518995@qq.com>
2023-07-15 10:47:24 +0800

    loca

---
+++ code/alimama/config.h
@@ -0,0 +1,8 @@
+#include <string>
+
+// ETCD 
+const std::string ECTD_URL = "http://etcd:2379";
+const std::string modelservice_key = "/services/searchservice";
+const std::string filename = "/data/data.csv";
+
+constexpr size_t total_data_num = 200000000;
